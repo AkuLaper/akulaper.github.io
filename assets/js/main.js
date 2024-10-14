59fb0871264882c6302b9cd7c3435d1d788b9603
GilangAndhika <gryzlegrizz@gmail.com>
2024-10-14 14:25:09 +0700

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// Mendapatkan tahun saat ini
+const currentYear = new Date().getFullYear();
+
+// Menampilkan tahun di elemen dengan id 'currentYear'
+document.getElementById('currentYear').textContent = currentYear;
